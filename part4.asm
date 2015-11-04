abe5b610a434e025e526aa3cead3d01ad86b5fe0
Marcus Motill <marcusmotill@gmail.com>
2015-11-03 19:36:27 -0500

    working FADD and FSUB

--- part4.asm
+++ part4.asm
@@ -4,18 +4,15 @@ START:     NOP
 
 	MOVE.B    #24,D1
 	TRAP      #0
-	FMOVE.P   $4000,FP0
-	MOVE.L    #0,$4000
-	MOVE.L    #0,$4004
-	MOVE.L    #0,$4008
+	*FMOVE.P   $4000,FP0
+	*MOVE.L    #0,$4000
+	*MOVE.L    #0,$4004
+	*MOVE.L    #0,$4008
 
-	MOVE.B    #24,D1
-	TRAP      #0
-	FMOVE.P   $4000,FP1
-
-         FADD      FP1,FP0
-
-         FMOVE.P   FP0,$4000
-
-	TRAP	  #15
+	*MOVE.B    #24,D1
+	*TRAP      #0
+	*FMOVE.P   $4000,FP1
+           *FADD      FP1,FP0
+           *FMOVE.P   FP0,$4000
+	TRAP      #15
 		END	START		; last line of source
--- testProg.asm
+++ testProg.asm
@@ -1,6 +1,10 @@
 		ORG	$4C00		;EXAMPLE PROGRAM
 START:     NOP
-	FMOVE.L     (A1),FP1
+	MOVE.B    #24,D1
+	TRAP      #0
+	FMOVE.P   $4000,FP1
+           FADD      FP1,FP0
+           FMOVE.P   FP0,$4000
 	NOP
 	TRAP	#14
 	TRAP	#15
